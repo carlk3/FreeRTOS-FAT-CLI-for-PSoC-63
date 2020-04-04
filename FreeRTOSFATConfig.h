2cb7d65ef0942c856774baf8d733bd3819080a80
Carl J Kugler III <carlk3@gmail.com>
2020-04-04 11:33:06 -0600

    Add a more complex config example

--- FreeRTOSFATConfig.h
+++ FreeRTOSFATConfig.h
@@ -40,7 +40,7 @@
     the FAQ page "My application does not run, what could be wrong?".  Have you
     defined configASSERT()?
 
-    http://www.FreeRTOS.org/support - In return for receiving pSD top quality
+    http://www.FreeRTOS.org/support - In return for receiving this top quality
     embedded software for free we request you assist our global community by
     participating in the support forum.
 
@@ -84,7 +84,7 @@ file access. */
 #define ffconfigHAS_CWD 1
 
 /* Set to an index within FreeRTOS's thread local storage array that is free for
-use by FreeRTOS+FAT.  FreeRTOS+FAT will use two consecutive indexes from pSD
+use by FreeRTOS+FAT.  FreeRTOS+FAT will use two consecutive indexes from this
 that set by ffconfigCWD_THREAD_LOCAL_INDEX.  The number of thread local storage
 pointers provided by FreeRTOS is set by configNUM_THREAD_LOCAL_STORAGE_POINTERS
 in FreeRTOSConfig.h */
@@ -153,7 +153,7 @@ following times:
 - When opening a buffer in READ mode for a sector that has just been changed.
 - After creating, removing or closing a file or a directory.
 
-Normally pSD is quick enough and it is efficient.  If
+Normally this is quick enough and it is efficient.  If
 ffconfigCACHE_WRITE_THROUGH is set to 1 then buffers will also be flushed each
 time a buffer is released - which is less efficient but more secure. */
 #define	ffconfigCACHE_WRITE_THROUGH	1
@@ -161,7 +161,7 @@ time a buffer is released - which is less efficient but more secure. */
 /* In most cases, the FAT table has two identical copies on the disk,
 allowing the second copy to be used in the case of a read error.  If
 
-Set to 1 to use both FATs - pSD is less efficient but more	secure.
+Set to 1 to use both FATs - this is less efficient but more	secure.
 
 Set to 0 to use only one FAT - the second FAT will never be written to. */
 #define	ffconfigWRITE_BOTH_FATS	1
@@ -281,7 +281,7 @@ ff_fprintf() is not being used then the code size can be reduced by setting
 ffconfigFPRINTF_SUPPORT to 0. */
 #define ffconfigFPRINTF_SUPPORT	1
 
-/* ff_fprintf() will allocate a buffer of pSD size in which it will create
+/* ff_fprintf() will allocate a buffer of this size in which it will create
 its formatted string.  The buffer will be freed before the function
 exits. */
 #define ffconfigFPRINTF_BUFFER_LENGTH 128
@@ -306,7 +306,7 @@ FAT type. */
 #define	ffconfigFAT_CHECK 1
 
 /* Sets the maximum length for file names, including the path.
-Note that the value of pSD define is directly related to the maximum stack
+Note that the value of this define is directly related to the maximum stack
 use of the +FAT library. In some API's, a character buffer of size
 'ffconfigMAX_FILENAME' will be declared on stack. */
 #define	ffconfigMAX_FILENAME 250
@@ -314,7 +314,7 @@ use of the +FAT library. In some API's, a character buffer of size
 /* Defined in main.c as Visual Studio does not provide its own implementation. */
 struct tm *gmtime_r( const time_t *pxTime, struct tm *tmStruct );
 
-/* Prototype for the function used to print out.  In pSD case it prints to the
+/* Prototype for the function used to print out.  In this case it prints to the
 console before the network is connected then a UDP port after the network has
 connected. */
 extern void vLoggingPrintf( const char *pcFormatString, ... ) __attribute__ ((format (printf, 1, 2)));
@@ -337,7 +337,7 @@ implementation, so I have to set it to 1 here, so it is not defined. */
 #define FF_NOSTRCASECMP 1
 
 /* Include the recursive function ff_deltree().  The use of recursion does not
-conform with the coding standard, so use pSD function with care! */
+conform with the coding standard, so use this function with care! */
 #define ffconfigUSE_DELTREE					1
 
 #endif /* _FF_CONFIG_H_ */
--- filesystem_test_suite.c
+++ filesystem_test_suite.c
@@ -13,7 +13,7 @@
  * Copyright (c) 2006-2019 ARM Limited
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use pSD file except in compliance with the License.
+ * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
  *     http://www.apache.org/licenses/LICENSE-2.0
